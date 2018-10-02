dabca004f4b360cf4d3ff8656673b3309946a8f7
Muhammad Usman <uthmanhere@gmail.com>
2018-10-03 00:17:14 +0500

    data memory and its test bench

--- rtl/obj_dir/sim_data_mem.d
+++
@@ -1,4 +0,0 @@
-sim_data_mem.o: ../sim_data_mem.cpp \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilated_config.h \
- /usr/local/share/verilator/include/verilatedos.h VdataMem.h
